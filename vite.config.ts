5ace0fd0f5dfee9290c60fb2b3d201718c7b749a
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2025-03-22 15:38:05 +0500

    build pro

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Agar GitHub subdomain bo‘lsa: base: '/repo-name/'
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
