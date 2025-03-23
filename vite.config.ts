a701f7cf0a23834075b8a670321fe736444dc672
Jamshid Zayniyev <84299973+jamshid-zayniyev@users.noreply.github.com>
2025-03-23 11:40:57 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Agar GitHub subdomain bo‘lsa: base: '/repo-name/'
+  base: '/stanfordlc/', // Agar GitHub subdomain bo‘lsa: base: '/repo-name/'
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
